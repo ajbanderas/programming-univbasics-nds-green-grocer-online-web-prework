70619d7b57afab14e5265bfd1f260fa80b79ba3d
Kevin Downs <kevin.downs04@gmail.com>
2019-10-28 04:24:06 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -69,7 +69,7 @@ def apply_clearance(cart)
     clearance = cart[index][:clearance]
     price = cart[index][:price]
     if clearance == true
-      price = (price * 0.8)
+      price = (price * (0.8))
       pp price
     end
   index += 1
